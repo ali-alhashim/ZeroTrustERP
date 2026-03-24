5610b37df3eda67ccd2fa89f66416d57e020f88e
Ali Alhashim <ali-alhashim@outlook.com>
2026-03-24 21:46:13 +0300

    ORM

--- apps/users/models/user.go
+++ apps/users/models/user.go
@@ -1,11 +1,33 @@
 package models
+import "time"
 
 type User struct {
-    ID        int
-    Email     string
-    Username  string
-    Active    bool    // if user not active, then user cannot login
-    OTPHash   string
-    OTPExpiry int64
-    Online    bool //user becomes online after verification
+    ID        int     `f:"number, primary, auto"`
+    Email     string  `f:"text, unique"`
+    Username  string  `f:"text, unique, notnull"`
+    Active    bool    `f:"bool, default:true"` // if user not active, then user cannot login
+    OTPHash   string  `f:"text"`
+    OTPExpiry int64   `f:"number"`
+    Online    bool    `f:"bool, default:false"` //user becomes online after verification
+    Roles     []Role  `f:"many2many:user_roles"` // many to many relationship with roles One user → many roles , One role → many users
+}
+
+type Role struct {
+	ID          int     `f:"number, primary, auto"`
+	Name        string  `f:"text, unique, notnull"`
+	Description string  `f:"text"`
+	Permissions []Permission `f:"many2many:role_permissions"`
+	CreatedAt   time.Time `f:"timestamp, default:current_timestamp"`
+	UpdatedAt   time.Time `f:"timestamp, default:current_timestamp"`
+}
+
+
+//Permissions read R, write W, delete D, update U,  all A
+type Permission struct {
+	ID     int     `f:"number, primary, auto"`
+	resource   string  `f:"text, unique, notnull"` // resource can be the name of the model or the name of the API endpoint or the name of the action
+	Action string  `f:"text, unique, notnull"` // action can be R, W, D, U, A
+    Description string  `f:"text"`
+    CreatedAt   time.Time `f:"timestamp, default:current_timestamp"`
+    UpdatedAt   time.Time `f:"timestamp, default:current_timestamp"`
 }
