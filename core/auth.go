6f05bf059bb37877116e363e74d392cd4cc34419
Ali Alhashim <ali-alhashim@outlook.com>
2026-03-24 22:14:22 +0300

    update auth

--- core/auth.go
+++ core/auth.go
@@ -1,10 +1,14 @@
 package core
 
 import (
-	"crypto/rand"
+	
 	"encoding/hex"
 	"net/http"
 	"time"
+	"crypto/sha256"
+	
+	"fmt"
+	"math/rand"
 )
 
 // generateSecureToken creates a random secure token
@@ -27,6 +31,16 @@ func CreateSession(w http.ResponseWriter, email string) {
 		SameSite: http.SameSiteStrictMode,
 	}
 	http.SetCookie(w, cookie)
+}
+
+
+func GenerateOTP() string {
+	return fmt.Sprintf("%06d", rand.Intn(1000000))
+}
+
+func HashOTP(otp string) string {
+	hash := sha256.Sum256([]byte(otp))
+	return hex.EncodeToString(hash[:])
 }
 
 
