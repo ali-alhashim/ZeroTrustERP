8ab951732ee6a31b50089cbb851c08e0c253ced1
Ali Alhashim <ali-alhashim@outlook.com>
2026-04-05 22:04:16 +0300

    update employees

--- apps/employees/models/employee.go
+++ apps/employees/models/employee.go
@@ -1,5 +1,7 @@
 package models
 
+import "time"
+
 
 
 type Employee struct {
@@ -9,7 +11,22 @@ type Employee struct {
     Department *Department `f:"many2one:"`   
     LocalName  string     `f:"text"`
     JobTitle   *JobTitle   `f:"many2one:"`   
+
+    CreatedAt   time.Time `f:"timestamp, default:current_timestamp"`
+    UpdatedAt   time.Time `f:"timestamp, default:current_timestamp"`
 }
+
+// OrgUnit represents a top-level organizational unit that can contain multiple departments
+type OrgUnit struct {
+    ID        int        `f:"number, primary, auto"`
+    Name      string     `f:"text, unique, notnull"`
+    Code      string     `f:"text, unique, notnull"`
+    Departments []Department `v:"true"`
+    CreatedAt   time.Time `f:"timestamp, default:current_timestamp"`
+    UpdatedAt   time.Time `f:"timestamp, default:current_timestamp"`
+    Manager  *Employee    `f:"one2one:employees"`
+}
+
 
 type Department struct {
     ID        int        `f:"number, primary, auto"`
@@ -17,6 +34,18 @@ type Department struct {
     Code      string     `f:"text, unique, notnull"`
     Employees []Employee `v:"true"`
     Manager   *Employee  `f:"one2one:employees"` 
+    CreatedAt   time.Time `f:"timestamp, default:current_timestamp"`
+    UpdatedAt   time.Time `f:"timestamp, default:current_timestamp"`
+}
+
+// ExManagerDepartment is a join table to track historical manager assignments for departments
+type ExManagerDepartment struct {
+    ID         int        `f:"number, primary, auto"`
+    Employee   *Employee  `f:"many2one:employees"`
+    Department *Department `f:"many2one:departments"`
+    CreatedAt   time.Time `f:"timestamp, default:current_timestamp"`
+    From   time.Time `f:"timestamp,"`
+    To   time.Time `f:"timestamp,"`
 }
 
 type JobTitle struct {
@@ -25,4 +54,6 @@ type JobTitle struct {
     Code        string `f:"text, unique, notnull"`
     Description string `f:"text"`
     Employees []Employee `v:"true"`
+    CreatedAt   time.Time `f:"timestamp, default:current_timestamp"`
+    UpdatedAt   time.Time `f:"timestamp, default:current_timestamp"`
 }
