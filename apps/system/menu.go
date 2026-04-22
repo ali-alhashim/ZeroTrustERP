12b2ae6ef3ef90fac23c819d4dc6802d627c2dd5
Ali Alhashim <ali-alhashim@outlook.com>
2026-04-22 20:44:29 +0300

    deletePermission

---
+++ apps/system/menu.go
@@ -0,0 +1,23 @@
+package system
+
+import (
+	"encoding/json"
+	"os"
+
+	"zerotrusterp/core"
+)
+
+func init() {
+	file, err := os.ReadFile("apps/system/menu.json")
+	if err != nil {
+		panic(err)
+	}
+
+	var items []core.MenuItem
+	err = json.Unmarshal(file, &items)
+	if err != nil {
+		panic(err)
+	}
+
+	core.RegisterMenus(items)
+}
